c1e6346ffe31b7cac23d3a47b8aa3fba46926afd
essob <aronzzon@hotmail.com>
2014-04-16 13:27:52 +0200

    Changed to be something else

--- ClientController.java
+++ ClientController.java
@@ -3,53 +3,37 @@ package Client1;
 import java.io.*;
 
 public class ClientController {
-    private ClientGUI gui;
-    private ClientConnection connection;
+	//    private ClientGUI gui;
+	private ClientConnection connection;
 
-    public ClientController(String serverIP, int serverPort) {
-        try {
-            gui = new ClientGUI();
-            connection = new ClientConnection(this, serverIP, serverPort);
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-    }
+	public ClientController(String serverIP, int serverPort) {
+		try {
+			//            gui = new ClientGUI();
+			connection = new ClientConnection(this, serverIP, serverPort);
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
 
-    public void newRequest(String request) {
-        double minPrice, maxPrice;
-        try {
-            String[] parts = request.split(",");
-            System.out.println(parts.length);
-            if (parts[0].equals("ALL")) {
-                connection.newRequest(new Request(parts[0]));
-            } else if (parts[0].equals("PRICE")) {
-                minPrice = Double.parseDouble(parts[1]);
-                maxPrice = Double.parseDouble(parts[2]);
-                connection.newRequest(new Request(parts[0], minPrice, maxPrice));
-            } else if (parts[0].equals("EXIT")) {
-                exit();
-            } else {
+	public void newRequest(String request) {
+	}
 
-            }
-        } catch (Exception e) {
-        }
-    }
+	public void exit() {
+		connection.exit();
+		System.exit(0);
+	}
 
-    public void exit() {
-        connection.exit();
-        System.exit(0);
-    }
-    
-    public void newResponse(Response response) {
-        Commodity[] commodities = response.getCommodities();
-        String message = "Request: " + response.getRequest() + "\n\n";
-        for(Commodity commodity : commodities) {
-            message += commodity.toString() + "\n";
-        }
-        gui.setResponse(message);
-    }
-    
-    public static void main(String[] args) {
-        new ClientController("127.0.0.1", 7654);
-    }
+	public void newResponse(Response response) {
+		String [] cards = response.getCards();
+		String message = "Request: " + response.getRequest() + "\n\n";
+		for(String card : cards) {
+			message += card + "\n";
+		}
+		//        gui.setResponse(message);
+		System.out.println(message);
+	}
+
+	public static void main(String[] args) {
+		new ClientController("127.0.0.1", 7766);
+	}
 }
--- Request.java
+++ Request.java
@@ -2,30 +2,14 @@ package Client1;
 import java.io.*;
 
 public class Request implements Serializable {
-    private static final long serialVersionUID = 1L;
-    private String request;
-    private double minPrice;
-    private double maxPrice;
-    
-    public Request(String request) {
-        this.request = request;
-    }
-    
-    public Request(String request, double minPrice, double maxPrice) {
-        this.request = request;
-        this.minPrice = minPrice;
-        this.maxPrice = maxPrice;
-    }
+	private static final long serialVersionUID = 1L;
+	private String request;
 
-    public String getRequest() {
-        return request;
-    }
+	public Request(String request) {
+		this.request = request;
+	}
 
-    public double getMinPrice() {
-        return minPrice;
-    }
-
-    public double getMaxPrice() {
-        return maxPrice;
-    }
+	public String getRequest() {
+		return request;
+	}
 }
