67e2ca9028dfe00522be702be6f20b40397d61f5
Elmseld <emily.elmseld@gmail.com>
2014-04-28 14:42:40 +0200

    skapat playlabel

---
+++ PlayLabel.java
@@ -0,0 +1,30 @@
+package Client1;
+
+import java.awt.Graphics;
+
+import javax.swing.JLabel;
+
+public class PlayLabel extends JLabel{
+	private JLabel ruter7 = new JLabel();
+	private JLabel ruterBigger = new JLabel();
+	private JLabel ruterSmaler = new JLabel();
+	
+	private JLabel klover7 = new JLabel();
+	private JLabel kloverBigger = new JLabel();
+	private JLabel kloverSmaler = new JLabel();
+	
+	private JLabel Heart7 = new JLabel();
+	private JLabel HeartBigger = new JLabel();
+	private JLabel HeartSmaler = new JLabel();
+	
+	private JLabel Spade7 = new JLabel();
+	private JLabel SpadeBigger = new JLabel();
+	private JLabel SpadeSmaler = new JLabel();
+	
+
+	protected void paintComponent(Graphics g) {
+	super.paintComponent(g);	
+	}
+
+
+}
